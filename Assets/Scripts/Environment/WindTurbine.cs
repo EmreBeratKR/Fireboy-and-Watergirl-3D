40ac37792097576487c9e3b86b54a67210ddb6e6
Emre Berat Kıran <doctoremre7@gmail.com>
2022-01-18 15:28:36 +0300

    Level 9 added
    -Now Wind Turbines can be toggled
    -In Level 3 additional Lever for the Wind Turbines added

--- Assets/Scripts/Environment/WindTurbine.cs
+++ Assets/Scripts/Environment/WindTurbine.cs
@@ -5,22 +5,72 @@ using UnityEngine;
 public class WindTurbine : MonoBehaviour
 {
     [SerializeField] private Transform blades;
+    [SerializeField] private Switch[] switches;
+    [SerializeField] private SwitchMode mode;
     [SerializeField] private float windForce;
+    [SerializeField] private bool isActive;
+    [SerializeField] private RotationAxis rotationAxis;
 
 
     private void Start()
     {
-        StartCoroutine(RotateCo());
+        if (isActive)
+        {
+            StartCoroutine(RotateCo());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (State())
+        {
+            if (!isActive)
+            {
+                isActive = true;
+                StartCoroutine(RotateCo());
+            }
+        }
+        else
+        {
+            if (isActive)
+            {
+                isActive = false;
+                StopAllCoroutines();
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" || other.tag == "Box")
+        if (isActive)
         {
-            other.attachedRigidbody.AddForce(transform.up * windForce, ForceMode.Impulse);
+            if (other.tag == "Player" || other.tag == "Box")
+            {
+                other.attachedRigidbody.AddForce(transform.up * windForce, ForceMode.Impulse);
+            }
         }
     }
 
+    private bool State()
+    {
+        if (mode == SwitchMode.One)
+        {
+            for (int i = 0; i < switches.Length; i++)
+            {
+                if (switches[i].isOn) return true;
+            }
+            return false;
+        }
+        else if (mode == SwitchMode.All)
+        {
+            for (int i = 0; i < switches.Length; i++)
+            {
+                if (!switches[i].isOn) return false;
+            }
+            return true;
+        }
+        return false;
+    }
 
     private IEnumerator RotateCo()
     {
@@ -29,15 +79,30 @@ public class WindTurbine : MonoBehaviour
 
         while (true)
         {     
-            blades.LeanRotateY(90f, duration * 0.25f);
-            yield return stepDuration;
-            blades.LeanRotateY(180f, duration * 0.25f);
-            yield return stepDuration;
-            blades.LeanRotateY(270f, duration * 0.25f);
-            yield return stepDuration;
-            blades.LeanRotateY(0f, duration * 0.25f);
-            yield return stepDuration;
+            if (rotationAxis == RotationAxis.Y)
+            {
+                blades.LeanRotateY(90f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateY(180f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateY(270f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateY(0f, duration * 0.25f);
+                yield return stepDuration;
+            }
+            if (rotationAxis == RotationAxis.X)
+            {
+                blades.LeanRotateX(90f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateX(180f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateX(270f, duration * 0.25f);
+                yield return stepDuration;
+                blades.LeanRotateX(0f, duration * 0.25f);
+                yield return stepDuration;
+            }
         }
-
     }
 }
+
+public enum RotationAxis {X, Y}
