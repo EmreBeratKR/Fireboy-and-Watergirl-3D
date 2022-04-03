ed440040fbfe6fe838be2d2725363b9cbcafc5f1
Emre Berat Kıran <doctoremre7@gmail.com>
2022-04-04 01:49:59 +0300

    Main Menu redesigned
    - Game Title added
    - Background changed
    - Source Code Button added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OpenSourceCode()
+    {
+        Application.OpenURL("https://github.com/EmreBeratKR/Fireboy-and-Watergirl-3D");
+    }
+}
